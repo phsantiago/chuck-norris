ea8dd49bdca1834b074758eaf816f84220e2d668
phsantiago <pedroh_s_@hotmail.com>
2018-01-24 02:05:50 +0000

    simple header done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Header.css';
+import ic from '../../img/norris-ic.png';
+
+function Header() {
+  return (
+    <div className={styles.base}>
+      <img src={ic} className={styles.ic} alt="ícone representando chuck norris"/>
+      <h1 className={styles.txtBg}>Chuck Norris</h1>
+      <h1 className={styles.txtBg}>Phrass Generator</h1>
+    </div>
+  );
+}
+
+
+export default Header;
