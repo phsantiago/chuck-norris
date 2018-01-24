15485d1a3b33e89aab47da5d1d3b7c717d8cfe77
phsantiago <pedroh_s_@hotmail.com>
2018-01-24 02:11:02 +0000

    WIP - Category component

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Categories.css';
+
+class Categories extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+    list: PropTypes.arrayOf(PropTypes.string),
+    onChoose: PropTypes.func,
+  };
+
+  render() {
+    return (
+      <div className={styles.base}>
+        <ul className={styles.listHolder}>
+          {
+            this.props.list.map((cat, i) => (
+              <li
+                className={styles.listItem}
+                onClick={() => this.props.onChoose(cat) }
+                key={i}>
+                { cat }
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Categories;
